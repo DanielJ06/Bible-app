522d45e63c7545f416a1ff0ab377c703141e244e
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-06-30 21:45:47 -0300

    Async storage

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,4 +1,5 @@
 import React, {createContext, useState, useEffect} from 'react';
+import {AsyncStorage} from 'react-native';
 
 export const AuthContext = createContext({});
 
@@ -7,29 +8,30 @@ import api from '../services/api';
 function AuthProvider({children}) {
   const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-  //   async function loadStorage() {
-  //     const userStorage = await AsyncStorage.getItem('Auth_user');
+  useEffect(() => {
+    async function loadStorage() {
+      const userStorage = await AsyncStorage.getItem('Auth_user');
 
-  //     if (userStorage) {
-  //       setUser(JSON.parse(userStorage));
-  //     }
-  //   }
-  //   loadStorage();
-  // }, []);
+      if (userStorage) {
+        setUser(JSON.parse(userStorage));
+      }
+    }
+    loadStorage();
+  }, []);
 
   async function signIn(values) {
     const response = await api.put('users/token', values);
     setUser(response.data);
+    storageUser(response.data);
   }
 
-  // async function signOut() {
-  //   await AsyncStorage.clear().then(() => {
-  //     setUser(null);
-  //     setSocket(null);
-  //     setLoading(false);
-  //   });
-  // }
+  async function storageUser(data) {
+    AsyncStorage.setItem('Auth_user', JSON.stringify(data));
+  }
+
+  async function signOut() {
+    await AsyncStorage.clear().then(() => setUser(null));
+  }
 
   return (
     <AuthContext.Provider
